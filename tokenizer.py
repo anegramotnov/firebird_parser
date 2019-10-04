09854c5add0fcfd7115f907e410faaa584e61298
Alexey Negramotnov <anegramotnov@gmail.com>
2019-10-04 13:24:00 +0400

    Test flake8 in CI

--- tokenizer.py
+++ tokenizer.py
@@ -1,4 +1,4 @@
-from typing import NamedTuple, List, Tuple, Iterator, Optional, Pattern, Callable
+from typing import NamedTuple, List, Tuple, Iterator, Optional, Pattern, Callable, Match
 import re
 
 
