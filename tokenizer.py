09b4b7931a982e14cfe06ecc00d4401cbd943c66
Алексей Неграмотнов <a.negramotnov@litebox.ru>
2019-10-04 13:21:34 +0400

    Add flake8 checks in travis CI

--- tokenizer.py
+++ tokenizer.py
@@ -1,4 +1,4 @@
-from typing import NamedTuple, List, Tuple, Iterator, Optional, Pattern, Match, Callable
+from typing import NamedTuple, List, Tuple, Iterator, Optional, Pattern, Callable
 import re
 
 
