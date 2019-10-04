5287793cdfc1e76b58e6554988050142d430ecaf
Alexey Negramotnov <anegramotnov@gmail.com>
2019-10-04 13:25:50 +0400

    Remove unused imports

--- tokenizer.py
+++ tokenizer.py
@@ -1,4 +1,4 @@
-from typing import NamedTuple, List, Tuple, Iterator, Optional, Pattern, Callable, Match
+from typing import NamedTuple, List, Tuple, Iterator, Optional, Pattern, Callable
 import re
 
 
